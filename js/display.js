e7d3e79444ee07e3d6aa683c56edf3006e585d11
Vincent Choqueuse <vincent.choqueuse@gmail.com>
2016-02-28 23:09:29 +0100

    test

--- js/display.js
+++ js/display.js
@@ -1,19 +1,21 @@
-$("#visualisation").change(function() {
-       domain=$( "#visualisation option:selected" ).text();
-       if (domain=="Time")
-       {
-       plot_chart.series[0].update({pointInterval:1./Fe});
-       plot_chart.xAxis[0].setExtremes(null,analyser.fftSize/Fe);
-       plot_chart.xAxis[0].update({title:{text: "Time (s)"}});
-       }
-       if (domain=="Frequency")
-       {
-       plot_chart.series[0].update({pointInterval:Fe/analyser.fftSize});
-       plot_chart.xAxis[0].setExtremes(null,Fe/2);
-       plot_chart.xAxis[0].update({title:{text: "Frequency (Hz)"}});
-       }
-       draw();
-       });
+$(function() {
+    $("#visualisation").change(function() {
+           domain=$( "#visualisation option:selected" ).text();
+           if (domain=="Time")
+           {
+           plot_chart.series[0].update({pointInterval:1./Fe});
+           plot_chart.xAxis[0].setExtremes(null,analyser.fftSize/Fe);
+           plot_chart.xAxis[0].update({title:{text: "Time (s)"}});
+           }
+           if (domain=="Frequency")
+           {
+           plot_chart.series[0].update({pointInterval:Fe/analyser.fftSize});
+           plot_chart.xAxis[0].setExtremes(null,Fe/2);
+           plot_chart.xAxis[0].update({title:{text: "Frequency (Hz)"}});
+           }
+           draw();
+           });
+  }
 
 
 //DRAW
